64934bae1aa88889351df483e3d0a0ef72022cf4
Nickolai Belakovski <nbelakovski@gmail.com>
2024-06-21 20:33:28 -0400

    Remove the need to even capture anything

--- algorithm.f90
+++ algorithm.f90
@@ -13,22 +13,20 @@ module algorithm
     
     contains
     
-    subroutine algorithm_with_nested_function(objective_function)
-        procedure(OBJ) :: objective_function
+    subroutine algorithm_with_nested_function()
+        call nested_function()  ! This works
         call evaluate(nested_function)
-
         contains
         
         subroutine nested_function()
-            call objective_function()
+            print *, "nested function"
         end subroutine nested_function
     
     end subroutine algorithm_with_nested_function
 
     subroutine evaluate(a_nested_function)
         procedure(OBJ) :: a_nested_function
-        call a_nested_function()
+        call a_nested_function()  ! This fails
     end subroutine evaluate
     
-    
-    end module algorithm
+end module algorithm
--- main.f90
+++ main.f90
@@ -1,20 +1,8 @@
-module progmod
-
-    public :: objective_function
-
-    contains
-
-    subroutine objective_function()
-    end subroutine objective_function
-
-end module progmod
-
 program hello
 
 use algorithm, only : algorithm_with_nested_function
-use progmod, only : objective_function
 
-call algorithm_with_nested_function(objective_function)
+call algorithm_with_nested_function()
 print *, "DONE"
 
 end program hello
